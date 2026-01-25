58757dcc63e2cddd8d59626ac7c814a07aad39a6
allhandsondeck <utkukopuz.nl@outlook.com>
2026-01-25 15:01:44 +0100

    add code snippet for pair exercise

---
+++ Week 3 - Functions and strings/2. Pair Exercise/userProfileUtil.js
@@ -0,0 +1,18 @@
+/*
+  Build functions that return:
+	•	Full name (capitalised): "John Doe"
+	•	Username: "john.doe"
+	•	Email: "john.doe@company.com"
+	•	Age
+	•	An object with above information in order
+
+  Requirements
+	•	Pure functions
+	•	Arrow functions
+	•	Template literals
+	•	One function = one job
+*/
+
+const firstName = "john";
+const lastName = "doe";
+const birthYear = 1995;
